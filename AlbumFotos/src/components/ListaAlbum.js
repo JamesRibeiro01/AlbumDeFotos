0c3ea284a37cfd2f1fc5b34e0d3a498bbfbb2f12
Thiago <thiagoworkspace@outlook.com>
2020-03-03 23:38:58 -0300

    gridTest

--- AlbumFotos/src/components/ListaAlbum.js
+++ AlbumFotos/src/components/ListaAlbum.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
-import { ScrollView, Text } from 'react-native';
-import axios from "axios";
-import Album from "./Album";
+import { View, Text, FlatList } from 'react-native';
+
+//create list data
+const dataList = [{key: '1'}, {key: '2'}, {key: '3'}, {key: '4'}, {key: '5'}, {key: '6'}, {key: '7'}, {key: '8'}, {key: '9'}, {key: '10'}]
+
 export default class ListaAlbum extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ListaItem: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
-    componentWillMount() {
+  _renderItem = ({item, index})=>{
+  <Text>{item.key}</Text>
+  }
 
-        axios.get('https://jsonplaceholder.typicode.com/photos')
-            .then(response => { this.setState({ ListaItem: response.data }) })
-            .catch(() => { alert('ERRO') });
-        //   axios.get('https://jsonplaceholder.typicode.com/photos')
-        //        .then(response => {console.log(response)})
-        //        .catch(()=>{alert('ERRO')})
-
-    }
-
-    render() {
-        return (
-            <ScrollView>
-                {this.state.ListaItem.map(item => (<Album propItem={item} />))}
-            </ScrollView>
-        );
-    }
+  render() {
+    return (
+      <View>
+        <FlatList data = {dataList} renderItem = {this._renderItem} keyExtractor = {(item, index) => index.toString()} />
+      </View>
+    );
+  }
 }
 
-//{this.state.ListaItem.map(item => (<Album key = {item.albumId} propItem = {item} />))}
+
+
+// import React, { Component } from 'react';
+// import { ScrollView, Text } from 'react-native';
+// import axios from "axios";
+// import Album from "./Album";
+// export default class ListaAlbum extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             ListaItem: []
+//         };
+//     }
+
+//     componentWillMount() {
+
+//         axios.get('https://jsonplaceholder.typicode.com/photos')
+//             .then(response => { this.setState({ ListaItem: response.data }) })
+//             .catch(() => { alert('ERRO') });
+
+//     }
+
+//     render() {
+//         return (
+//             <ScrollView>
+//                 {this.state.ListaItem.map(item => (<Album propItem={item} />))}
+//             </ScrollView>
+//         );
+//     }
+// }
+
