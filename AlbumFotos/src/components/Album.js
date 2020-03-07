14c2266c05027abb5000172f161813df630ca294
Thiago <thiagoworkspace@outlook.com>
2020-03-06 23:51:54 -0300

    estilos

--- AlbumFotos/src/components/Album.js
+++ AlbumFotos/src/components/Album.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default class Album extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
 
   render() {
     return (
-
-      <TouchableOpacity >
       <View style={Estilos.ContainerPrincipal}>
 
         <View style = {Estilos.containerThumb}>
 
           <View style={Estilos.imagemThumbnail}>
-            <Image style={{ width: 100, height: 100 }} source={{ uri: this.props.novaListaAlbum.thumbnailUrl }} />
+            <Image style={{ width: 100, height: 100 }} source={{ uri: this.props.imagemAlbum.thumbnailUrl }} />
           </View>
 
           <View>
-            <Text style={Estilos.titulo}>{this.props.novaListaAlbum.albumId}</Text>
+            <Text style={Estilos.titulo}>{this.props.imagemAlbum.albumId}</Text>
           </View>
 
         </View>
       </View>
-      </TouchableOpacity>
     );
   }
 }
--- AlbumFotos/src/components/ListaAlbum.js
+++ AlbumFotos/src/components/ListaAlbum.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, SafeAreaView, Image, FlatList, StatusBar, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StatusBar, Image, StyleSheet, TouchableOpacity, Platform, ActivityIndicator, ToastAndroid } from 'react-native';
 import Album from "./Album";
 import axios from 'axios';
 
@@ -9,28 +9,36 @@ export default class ListaAlbum extends Component {
     this.state = {
       listaFotos: []
     };
-  };
+  }
 
+  componentDidMount() {
 
-  componentWillMount() {
-    axios.get('https://jsonplaceholder.typicode.com/photos')
-      .then(response => { this.setState({ listaFotos: response.data }) })
+    return fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ listaFotos: responseJson });
+      }).catch((error) => { ToastAndroid.show(error.toString(), ToastAndroid.SHORT) });
   }
 
   render() {
     return (
-
-      <View>
+      <View style={Estilos.ContainerPrincipal}>
         <StatusBar backgroundColor='grey' />
         <FlatList data={this.state.listaFotos}
           renderItem={({ item }) => {
             return (
               <View>
-              <Image source={{uri: item.thumbnailUrl}} />
+
+                <TouchableOpacity>
+                  <View style = {Estilos.imagemUrl}>
+                    <Image style={{ width: 100, height: 100 }} source={{ uri: item.imagemUrl }} />
+                  </View>
+                </TouchableOpacity>
+
               </View>
             )
           }}
-          keyExtractor={(index) => { return index }}
+          keyExtractor={(item, index) => index.toString()}
           numColumns={3} />
       </View>
     );
@@ -38,3 +46,26 @@ export default class ListaAlbum extends Component {
 }
 
 
+const Estilos = StyleSheet.create({
+  ContainerPrincipal: {
+    justifyContent: 'center',
+    flex: 1,
+    paddingTop: 30,
+  },
+  imagemUrl: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  titulo: {
+    flex: 1,
+    fontSize: 14,
+    color: 'black'
+  },
+
+  containerThumb: {
+    flex: 1,
+    borderWidth: 1.0,
+    borderColor: '#CCC',
+  }
+});
